f1de769cc1fde41cd893311b1f6619464a95cdf2
Dhruv Gupta <druvgupta62@gmail.com>
2026-02-01 22:47:43 +0530

    Added JWT expiry

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -54,9 +54,12 @@ async function isAuthenticated(token){
         }
         return user.id;
     }catch(error){
-        if(error instanceof AppError)throw error;
-        if(error.name=="JsonWebTokenError"){
+        if(error instanceof AppError) throw error;
+        if(error.name=='JsonWebTokenError'){
             throw new AppError('Invalid JWT Token', StatusCodes.BAD_REQUEST);
+        }
+        if(error.name == 'TokenExpiredError'){
+             throw new AppError('JWT Token Expired', StatusCodes.BAD_REQUEST);
         }
         console.log(error);
         throw error;
